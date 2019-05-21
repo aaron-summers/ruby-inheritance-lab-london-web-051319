2d21f7a9c2135ed879ebf4b0b091781b36d5f719
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-21 12:58:30 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,8 +1,13 @@
 class Student < User
+  attr_accessor :array
   # new_array = Array.new(0)
   
   def initialize
-    array = Array.new(0)
+    @array = Array.new(0)
+  end
+  
+  def learn(string)
+    @array << string
   end
   
 end
