61e0accf55ef9de274d4144ecd2b4f7a0d4ad7c7
Akshat Vishwakarma <aaron.summers2797@gmail.com>
2019-05-21 13:58:56 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
-
+class Student < User
+  attr_accessor :knowledge
+ def initialize
+   @knowledge  = Array.new(0)
+ end
+ 
+ def learn(string)
+   @knowledge << string
+ end
+ 
+ def knowledge
+   @knowledge
+ end
+  
 end
